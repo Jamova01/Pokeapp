0314846c0b5c06c1d3af8a13351a66ab4025ae04
Jamova01 <jorgemova01@gmail.com>
2024-09-24 20:43:42 -0500

    add Home page with Pokémon list and search functionality

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,34 @@
+// src/page/Home.jsx
+import { useEffect } from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import { PokemonList } from "../../components/PokemonList";
+import { fetchPokemonsWithDetails } from "../../slices/dataSlice";
+import { Header } from "../../components/Header";
+import "./Home.css";
+
+const Home = () => {
+  const pokemons = useSelector((state) => state.data.pokemons, shallowEqual);
+  const searchQuery = useSelector(
+    (state) => state.data.searchQuery,
+    shallowEqual
+  );
+  const loading = useSelector((state) => state.ui.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPokemonsWithDetails());
+  }, [dispatch]);
+
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className="container">
+      <Header />
+      {loading ? <p>Loading</p> : <PokemonList pokemons={filteredPokemons} />}
+    </div>
+  );
+};
+
+export default Home;
