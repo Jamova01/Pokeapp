319b6aaccec38d5dd913c2c45555f3349b7d0bdb
Jamova01 <jorgemova01@gmail.com>
2024-09-24 23:45:20 -0500

    add Logo component to display the app logo

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,10 @@
+import "./Logo.css";
+
+export const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="./pokeball.png" alt="Pokeball" />
+      <img src="./Pokédex.png" alt="Pokédex" />
+    </div>
+  );
+};
